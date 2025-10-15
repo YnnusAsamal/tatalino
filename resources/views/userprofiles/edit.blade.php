ab217644a47de7c9e1da32ce70283da2d36855d4
Sunny C. Lamasa Jr. <46011418+YnnusAsamal@users.noreply.github.com>
2025-10-15 10:06:57 +0800

    Update edit.blade.php

--- resources/views/userprofiles/edit.blade.php
+++ resources/views/userprofiles/edit.blade.php
@@ -90,7 +90,7 @@
                 @endphp
 
                 @if($firstImage)
-                    <img src="{{ asset('assets/userprofiles/' . $firstImage) }}" alt="Current Image" class="rounded-profile mb-2">
+                    <img src="{{ asset('public/assets/userprofiles/' . $firstImage) }}" alt="Current Image" class="rounded-profile mb-2">
                 @else
                     <p>No profile image available.</p>
                 @endif
