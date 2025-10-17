22aa09c03c4522efbaf63a5be4d8096b186af27f
Sunny C. Lamasa Jr. <46011418+YnnusAsamal@users.noreply.github.com>
2025-10-17 15:36:20 +0800

    Update AuthorController.php

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\UserProfile;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class AuthorController extends Controller
 {
