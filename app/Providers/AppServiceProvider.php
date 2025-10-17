bcba581611425466599c6e613d0eea22a8e5d1aa
Sunny C. Lamasa Jr. <46011418+YnnusAsamal@users.noreply.github.com>
2025-10-17 12:00:14 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,5 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(125);
         Paginator::useBootstrap();
-        Paginator::useBootstrapFive();
     }
 }
