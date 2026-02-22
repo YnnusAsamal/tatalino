9a441daec7db1c4386e11bbe268318e774e18278
Sunny C. Lamasa Jr. <46011418+YnnusAsamal@users.noreply.github.com>
2026-02-22 13:24:02 +0800

    Update index.blade.php

--- resources/views/contacts/index.blade.php
+++ resources/views/contacts/index.blade.php
@@ -7,40 +7,33 @@
   <p>{{ $message }}</p>
 </div>
 @endif
-
-<div class ="py-12 mx-3 m-4">
-    <div class="card">
-        <div class ="card-header">
-            <div class="pull-left">
-                <h4>Contact/Inquiry Management</h4>
-            </div>
-                <div class="pull-right">
-                    <a class="btn btn-success btn-sm" href="{{ route('contacts.create') }}"> Create New Contact</a>
-                </div>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                @foreach ($contacts as $contact)
-                    <div class="col-md-4 mb-4">
-                        <div class="card h-100 shadow-sm">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $contact->name }}</h5>
-                                <h6 class="card-subtitle mb-2 text-muted">
-                                    {{ $contact->email }}
-                                </h6>
-                                <p class="card-text">
-                                    {{ $contact->message }}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                @endforeach
-            </div>
-        </div>
-        <div class="card-footer">
-            {!! $contacts->links() !!}
+<div class="container mt-3">
+    <div class="row">
+        <div class="col">
+            <h4>Contact Messages</h4>
         </div>
     </div>
+    <div class="row">
+        @foreach ($contacts as $contact)
+            <div class="col-md-4 mb-4">
+                <div class="card h-100 shadow-sm">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $contact->name }}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            {{ $contact->email }}
+                        </h6>
+                        <p class="card-text">
+                            {{ $contact->message }}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+    <div class="row"><div class="col">
+        {{ $contacts->links() }}
+    </div></div>
 </div>
+
 
 @endsection
